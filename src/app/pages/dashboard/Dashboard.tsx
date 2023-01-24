8cdbe3cff69b9a89be6aa0fc81c337e50cd931e8
Rafiere <rafapgbr@gmail.com>
2023-01-24 15:05:44 -0300

    feat: adding useRef example to store a value without re-rendering the UI.

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const Dashboard = () => {
@@ -8,10 +8,17 @@ export const Dashboard = () => {
     navigate("/login");
   };
 
+  /* O valor será atualizado, o estado, não, pois estamos realizando uma modificação direta. */
+  const counterRef = useRef(0);
+
   return (
     <div>
       <h1>Página de Dashboard</h1>
       <button onClick={handleClick}>Ir para Login</button>
+
+      <p>Contador: {counterRef.current}</p>
+      <button onClick={() => counterRef.current++}>Somar</button>
+      <button onClick={() => console.log(counterRef)}>Gerar Log</button>
     </div>
   );
 };
