377d78db298fee391b6bf98877e1c1ee4e11499d
Rafiere <rafapgbr@gmail.com>
2023-01-24 10:32:58 -0300

    feat: adding login page

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    console.log(email);
+    console.log(password);
+  };
+
   return (
     <div>
       <h1>Página de Login</h1>
       <Link to={"/dashboard"}>Ir para Dashboard</Link>
+
+      <form>
+        <label>
+          <span>Email</span>
+          <input value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <label>
+          <span>Password</span>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
     </div>
   );
 };
