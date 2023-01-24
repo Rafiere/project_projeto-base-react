d06e00a1e12325895fd3cb4aa3573d12d351d2c5
Rafiere <rafapgbr@gmail.com>
2023-01-24 09:56:13 -0300

    feat: adding login route

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Dashboard } from "../pages";
+import { Dashboard, Login } from "../pages";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<h1>Página não encontrada!</h1>} />
       </Routes>
     </BrowserRouter>
