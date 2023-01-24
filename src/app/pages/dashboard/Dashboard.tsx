b46399192a24d609b59b0a50733c277e8bc4a74c
Rafiere <rafapgbr@gmail.com>
2023-01-24 09:55:47 -0300

    feat: adding dashboard page.

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/login");
+  };
+
+  return (
+    <div>
+      <h1>Página de Dashboard</h1>
+      <button onClick={handleClick}>Ir para Login</button>
+    </div>
+  );
 };
