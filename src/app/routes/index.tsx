f33c488a3022c09e108f12a32a03547c41eb46fc
Rafiere <rafapgbr@gmail.com>
2023-01-25 20:03:33 -0300

    feat: adding "ComponentizedLogin" page component.

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Dashboard, Login } from "../pages";
+import ComponentizedLogin from "../pages/componentized-login/ComponentizedLogin";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/componentized-login" element={<ComponentizedLogin />} />
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<h1>Página não encontrada!</h1>} />
       </Routes>
